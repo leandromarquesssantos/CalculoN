01b4686736e72db1e559f89a605e5869fc001af4
Leandro <leandromarquesssantos@gmail.com>
2016-09-16 17:37:55 -0300

    Alteraçõs no script
    Alterações gerais no script Gauss_Jacobi

--- Python/gauss_jacobi.py
+++ Python/gauss_jacobi.py
@@ -18,7 +18,7 @@ def gauss_jacobi(A,B,k=0,x=None):
         x[0] = (1 / A[0][0])*(B[0] - (A[0][1]*x[1]) - (A[0][2]-x[2]))
         x[1] = (1 / A[1][1])*(B[1] - (A[1][0]*x[0]) - (A[1][2]-x[2]))
         x[2] = (1 / A[2][2])*(B[2] - (A[2][0]*x[0]) - (A[2][1]-x[1]))
-        k ++
+        k = k + 1
     print (k)
     return x
 
@@ -31,17 +31,41 @@ def gauss_jacobi(A,B,k=0,x=None):
 
 def erro(X,Xant,tolerado = 10**-2) :
     resultado = (X - Xant)
-    #print(type(resultado))
-    er = max(resultado) / max(X)
+    print(resultado)
+    print(range(len(resultado)))
+    maior1 = resultado[0][0]
+    maior2 = X[0]
+    for i in range(len(resultado)) :
+        for j in range(len(resultado[i]))
+           if (resultado[i] < 0) :
+               if (maior1 < (resultado[i][j]*-1)) :
+                   maior1 = (resultado[i][j]*-1)
+           if (maior1 < (resultado[i][j])) :
+                   maior1 = (resultado[i][j])
+        
+    for i in range(len(X)) :
+        
+            if X[i] < 0 :
+                if maior2 < (X[i]*-1) :
+                    maior2 = (X[i]*-1)
+            if maior1 < (X[i]) :
+                    maior2 = (X[i])
+    
+    
+    er = maior1 / maior2
     print (er)
-    #if er < tolerado :
-    #    return True
+    if er < tolerado :
+        return True
     return False
 
 
-A = array('f',[[10,3,-2],[2,8,-1],[1,1,5]])
+A = array([[10., 3.,-2.],
+           [ 2., 8.,-1.],
+           [ 1., 1., 5.]])
 
-B = array('f',[[57],[20],[-4]])
+B = array([[57.],
+           [20.],
+           [-4.]])
 
 print("Linhas: ",len(A))
 
