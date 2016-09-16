af9d715872fe1d91df696b65f08b8598f7c4e637
Leandro <leandromarquesssantos@gmail.com>
2016-09-16 18:40:10 -0300

    Alterações gerais
    def erro() funcionando

--- Python/gauss_jacobi.py
+++ Python/gauss_jacobi.py
@@ -12,14 +12,15 @@ def gauss_jacobi(A,B,k=0,x=None):
     
     #cria uma matriz do comprimento da matriz A preenchida com zeros    
     if x is None :
-        x = zeros(len(A))
+        x = zeros(len(A)) #x = [0,0,0,...]
         
-    if k == 0 :
+    while (erro(gauss_jacobi(A,B),x)) : 
         x[0] = (1 / A[0][0])*(B[0] - (A[0][1]*x[1]) - (A[0][2]-x[2]))
         x[1] = (1 / A[1][1])*(B[1] - (A[1][0]*x[0]) - (A[1][2]-x[2]))
         x[2] = (1 / A[2][2])*(B[2] - (A[2][0]*x[0]) - (A[2][1]-x[1]))
         k = k + 1
     print (k)
+    
     return x
 
 
@@ -30,33 +31,37 @@ def gauss_jacobi(A,B,k=0,x=None):
 """
 
 def erro(X,Xant,tolerado = 10**-2) :
-    resultado = (X - Xant)
+    resultado = array(X - Xant)
     print(resultado)
     print(range(len(resultado)))
     maior1 = resultado[0][0]
     maior2 = X[0]
     for i in range(len(resultado)) :
-        for j in range(len(resultado[i]))
-           if (resultado[i] < 0) :
-               if (maior1 < (resultado[i][j]*-1)) :
-                   maior1 = (resultado[i][j]*-1)
-           if (maior1 < (resultado[i][j])) :
-                   maior1 = (resultado[i][j])
+        for j in range(len(resultado[i])) :
+            var = float(resultado[i][j])
+            if (var < 0) :
+                var = var * -1
+            
+            if var > maior1 :
+                maior1 = var
+            
+            
         
     for i in range(len(X)) :
         
-            if X[i] < 0 :
-                if maior2 < (X[i]*-1) :
-                    maior2 = (X[i]*-1)
-            if maior1 < (X[i]) :
-                    maior2 = (X[i])
+        var = float(X[i])
+        if (var < 0) :
+            var = var * -1
+            
+        if var > maior2 :
+            maior2 = var
     
-    
+    print(maior1,maior2)
     er = maior1 / maior2
     print (er)
     if er < tolerado :
-        return True
-    return False
+        return False
+    return True
 
 
 A = array([[10., 3.,-2.],
@@ -84,11 +89,11 @@ print(B)
 print("X: ")
 print(gauss_jacobi(A,B))
 
-for i in range(len(gauss_jacobi(A,B))) :
-    print(gauss_jacobi(A,B).transpose()[i])
 
-print(max(gauss_jacobi(A,B)))
-
-erro(gauss_jacobi(A,B), A)
+if (erro(gauss_jacobi(A,B), A)):
+    print("True")
+else :
+    print("False")
+    
 
     
