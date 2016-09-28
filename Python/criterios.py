50f05330deadceae25408893300c03fcd0ca54b1
Leandro <leandromarquesssantos@gmail.com>
2016-09-28 18:05:15 -0300

    Adicionado Materia de Referencia
    alterações no script criterios sassenfeld

--- Python/criterios.py
+++ Python/criterios.py
@@ -7,13 +7,13 @@
 from numpy import array, zeros, amax
 
 def maximo(vetor,criterio) :
-    max = vetor[0]    
-    for i in range(len(vetor)) :    
+    max = vetor[0]
+    for i in range(len(vetor)) :
         if  vetor[i] > max :
             max = vetor[i]
-    
-    print (max)    
-    if max < criterio :    
+
+    print (max)
+    if max < criterio :
         return True
     else :
         return False
@@ -25,11 +25,11 @@ def criteriodaslinhas(A) :
         for j in range(len(A[i])) :
             if j != i :
                 if A[i][j] < 0 :
-                    sum = sum + (-1 * A[i][j])    
+                    sum = sum + (-1 * A[i][j])
                 else :
                     sum = sum + A[i][j]
         alfa[i] = sum / A[i][i]
-        
+
         return maximo(alfa,0)
 
 def criteriodascolunas(A) :
@@ -39,7 +39,7 @@ def criteriodascolunas(A) :
         for j in range(len(A[i])) :
             if j != i :
                 if A[i][j] < 0 :
-                    sum = sum + (-1 * A[i][j])    
+                    sum = sum + (-1 * A[i][j])
                 else :
                     sum = sum + A[i][j]
         beta[i] = sum / A[j][j]
@@ -49,19 +49,23 @@ def criteriodascolunas(A) :
 def criteriodesassenfeld(A) :
     alfa = 0
     beta = []
+
     for i in range(len(A)) :
-        if A[i][1] < 0 :
-            alfa = (alfa + (-1 * A[i][1]))
+        if i != 0 :
+            if A[0][i] < 0 :
+                alfa = (alfa + (-1 * A[0][i]))
+            else :
+                alfa = (alfa + A[0][i])
+        if alfa / A[0][0] < 0 :
+            resultado = (alfa * (1 / A[0][0]) * -1)
         else :
-            alfa = (alfa + A[i][1])
-    if alfa / A[0][0] < 0 :
-        resultado = (alfa / A[0][0]) * -1
-    else :
-        resultado = alfa / A[0][0]
+            resultado = alfa * (1 / A[0][0])
+
+
     beta.append(resultado)
-    
+
 #    return beta[0]
-    
+
     for i in range(len(A)) :
         alfa = 0
         if i == 0 :
@@ -69,16 +73,26 @@ def criteriodesassenfeld(A) :
             if resultado < 0 :
                 resultado = resultado * -1
             beta.append(resultado)
+
         else :
-            resultado = beta[i] * A[i][i] / A[i][i]
-            if resultado < 0 :
-                resultado = resultado * -1
-            alfa = 0
-            for n in range(len(A)) :
-                if alfa < 0 :
-                    alfa = alfa + (A[n][i] * -1)
-                else :
-                    alfa = alfa + A[n][i]
+            for s in range(len(beta)) :
+                for l in range(len(A[i]))
+                    alfa = 0
+                    if l == 0 :
+                        alfa = A[i][l] * beta[i]
+                    else :
+                        
+#            resultado = beta[i] * A[i][i] / A[i][i]
+#            if resultado < 0 :
+#                resultado = resultado * -1
+#            alfa = 0
+#            for n in range(len(A)) :
+#                if alfa < 0 :
+#                    alfa = alfa + (A[n][i] * -1)
+#                else :
+#                    alfa = alfa + A[n][i]
+
+
             if A[i][i] < 0 :
                 alfa = alfa / (A[i][i] * -1)
             else :
@@ -86,14 +100,14 @@ def criteriodesassenfeld(A) :
             if alfa < 0 :
                 resultado = resultado + (alfa * -1)
             else :
-                resultado = resultado + alfa    
+                resultado = resultado + alfa
             beta.append(resultado)
-    
-    return maximo(beta,0)
+    print(beta)
+    return maximo(beta,1)
 
-A = array([[3., 0., 1., 3.],
-           [1.,-1., 0., 1.],
-           [3., 1., 2., 9.]])
+A = array([[3., 0., 1.],
+           [1.,-1., 0.],
+           [3., 1., 2.]])
 
 #A = array([[5.,2.,0.],
 #           [1.,0.,5.],
@@ -120,5 +134,5 @@ else :
     print ('nao passou no criterio de sassenfeld')
 '''
 
-print (amax(A))
-print (maximo(A[2]))
+print (criteriodesassenfeld(A))
+print (maximo(A[2],1))
