2c035154bbaaf01cefdfe2f9228f4920f4842605
leandromarquesssantos <leandromarquesssantos@gmail.com>
2016-10-17 00:28:00 -0200

    Ac2-1
    acrescentado exercicio 1 da ac2

--- Python/gauss_jacobi.py
+++ Python/gauss_jacobi.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 """
 
-
-@author: Leandro
+@author: Leandro M
 """
 from numpy import array, zeros, dot, allclose
 #funcao que implementa o medoto Gauss Jacobi, recebendo como parametro matrizes A e B
 def gauss_jacobi(A,B,k=0,x=None) :
+    A = pivoteamento(A,B)
     #cria uma matriz do comprimento da matriz A preenchida com zeros
     #inicializa o vetor X com 0 em cada posição
     x = zeros(len(A)) #x = [0,0,0,...]
@@ -14,6 +14,8 @@ def gauss_jacobi(A,B,k=0,x=None) :
     continua = True
     #variavel contador do numero de interações
     interacoes = 0
+    #variavel para ser a saida no arquivo
+    saida = [] 
     #enquanto o valor encontrado for maior que o erro tolerado, continua com a interação enquanto o resultado da funcao erro for true
     while (continua) :
         #interacoes recebe interacoes + 1
@@ -33,11 +35,20 @@ def gauss_jacobi(A,B,k=0,x=None) :
         continua = erro(X, x) #utilizando a funcao erro que escrevi para calcular o valor erro
         print(interacoes)
         #impoe um limite de interações
-        if (interacoes == 10000) :
+        if (interacoes == 1000) :
             print("não encontrou")
             return x
         #salva o valor calculado X na variavel x
         x = X;
+        #para ir salvando os resultados da interação em um arquivo csv para abrir no excel
+        saida.append(str(interacoes)+';')    
+        for aux in range(len(X)) :
+            saida.append('{}'.format(X[aux]))
+            saida.append(';')
+        saida.append('\n')
+        arq = open("resultado.csv", 'w')        
+        arq.writelines(saida)
+        arq.close
     #retorna o vetor x como resultado
     return x
 
@@ -45,8 +56,8 @@ def gauss_jacobi(A,B,k=0,x=None) :
 def erro(X,Xant,tolerado = 10**-10) :
     #resultado armazena o vetor resultado interação atual - anterior
     resultado = array(X - Xant)
-    print(resultado)
-    print(range(len(resultado)))
+    #print(resultado)
+    #print(range(len(resultado)))
     #inicia a variavel com o primeiro elemento da matriz
     maior1 = resultado[0]
     #inicia a variavel com o primeiro elemento do vetor
@@ -76,11 +87,13 @@ def erro(X,Xant,tolerado = 10**-10) :
             #salva o valor da variavel na variavel maior2
             maior2 = var
 
-    print(maior1,maior2)
+    #print(maior1,maior2)
     #variavel er recebe o o valor da variavel maior1 dividido pela maior2
     er = maior1 / maior2
-    print (er)
+    #print (er)
     #se variavel er for menor que a variavel tolerado
+    #getcontext().prec = 6
+
     if er < tolerado :
         #retorna Falso
         return False
@@ -88,6 +101,36 @@ def erro(X,Xant,tolerado = 10**-10) :
     return True
 
 
+def pivoteamento(A,B):
+    n = len(A[0]) - 1
+    for j in range(len(A[0]) - 1):
+        i = 1
+        while i <= (n-j) :
+            #A[j][j] é o pivo
+            if abs(A[j+i][j]) > abs(A[j][j]) :
+                k = j
+                while (k <=n) :
+                    aux = A[j+i][k]
+                    A[j+i][k] = A[j][k]
+                    A[j][k] = aux
+                    k += 1
+                aux = B[j+i]
+                B[j+i] = B[j]
+                B[j] = aux
+            pivo = A[j][j]
+            primeiro = A[j+i][j]
+            if pivo != 0 :
+                B[j+i] = B[j+i] - (primeiro / pivo)*B[j]
+                z = j
+                while z <= n :
+                    A[j+i][z] = A[j+i][z] - (primeiro / pivo) * A[j][z]
+                    z += 1
+            i += 1
+        j += 1
+    #retorna o pivoteamento 
+    return A 
+
+'''
 A = array([[10., 3.,-2.],
            [ 2., 8.,-1.],
            [ 1., 1., 5.]])
@@ -95,10 +138,28 @@ A = array([[10., 3.,-2.],
 B = array([[57.],
            [20.],
            [-4.]])
+'''
+A = array([ [ 2., 1., 7., 4.,-3.,-1., 4., 4., 7., 0.],
+            [ 4., 2., 2., 3.,-2., 0., 3., 3., 4., 1.],
+            [ 3., 4., 4., 2., 1.,-2., 2., 1., 9.,-3.],
+            [ 9., 3., 5., 1., 0., 5., 6.,-5.,-3., 4.],
+            [ 2., 0., 7., 0.,-5., 7., 1., 0., 1., 6.],
+            [ 1., 9., 8., 0., 3., 9., 9., 0., 0., 5.],
+            [ 4., 1., 9., 0., 4., 3., 7.,-4., 1., 3.],
+            [ 6., 3., 1., 1., 6., 8., 3., 3., 0., 2.],
+            [ 6., 5., 0.,-7., 7.,-7., 6., 2.,-6., 1.],
+            [ 1., 6., 3., 4., 8., 3.,-5., 0.,-6., 0.] ])
 
-print("Linhas: ",len(A))
-
-print("Colunas: ",len(A[0]))
+B = array([ [ 86. ],
+            [ 45. ],
+            [52.5 ],
+            [108. ],
+            [66.5 ],
+            [90.5 ],
+            [139. ],
+            [ 61. ],
+            [-43.5],
+            [ 31.] ])
 
 
 print("A: ")
